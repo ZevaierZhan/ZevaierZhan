1be9f154852e501d8f0e237f6eb1921f51b9d90b
ZevaierZhan <zavierzhan@gmail.com>
2025-06-05 12:43:26 +0800

    个人服务器配置

---
+++ server/init_ssh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SSH_PORT=10022
+DISABLE_ROOT=true
+NEW_USER=zevaier
+
+echo "修改SSH配置..."
+sudo sed -i "s/^#Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
+if [ "$DISABLE_ROOT" = "true" ]; then
+    sudo sed -i "s/^PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+fi
+
+# 2. 创建新用户
+echo "创建新用户 $NEW_USER..."
+sudo useradd -m -s /bin/bash $NEW_USER
+sudo mkdir -p /home/$NEW_USER/.ssh
+sudo chmod 700 /home/$NEW_USER/.ssh
+
+# 3. 设置密钥认证
+echo "设置密钥认证..."
+sudo cp /root/.ssh/authorized_keys /home/$NEW_USER/.ssh/
+sudo chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh
+sudo chmod 600 /home/$NEW_USER/.ssh/authorized_keys
